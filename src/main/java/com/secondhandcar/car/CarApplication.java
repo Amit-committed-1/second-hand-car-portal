89a9e8ccbdef00b14d4bc2e01303980246ca2eaf
Amit-committed-1 <AmitBhanj76@gmail.com>
2025-07-21 11:48:07 +0530

    Update CarApplication.java
    some changes done in code for modification

--- src/main/java/com/secondhandcar/car/CarApplication.java
+++ src/main/java/com/secondhandcar/car/CarApplication.java
@@ -8,6 +8,9 @@ public class CarApplication {
 
 	public static void main(String[] args) {
                 int x = 1000;
+		String name = "Mike";
+		String email = "mike@gmail.com";
+		String mobile = "9337364533"
 		SpringApplication.run(CarApplication.class, args);
 	}
 
